519b16be0cf0ca6b24428a2d2f4b09f6fcc7261c
zurustar <zurustar@users.noreply.github.com>
2017-12-29 15:58:18 +0900

    modified

--- xylitol.go
+++ xylitol.go
@@ -1,60 +1,66 @@
 package main
 
 import (
-        "fmt"
-        "log"
-        "net"
-        "os"
-        "regexp"
-        "strings"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"regexp"
+	"strings"
 )
 
+var re = RegEx{}
+
+type RegEx struct{
+
+}
+
 type Header struct {
-        hname string
-        hvals []string
+	hname string
+	hvals []string
 }
 
 type Message struct {
-        method  string
-        requri  string
-        stcode  string
-        reason  string
-        headers []Header
-        body    string
+	method  string
+	requri  string
+	stcode  string
+	reason  string
+	headers []Header
+	body    string
 }
 
 func NewMessage() *Message {
-        return &Message{}
+	return &Message{}
 }
 
 func (this *Message) Parse(s *string, addr *net.UDPAddr) {
-        log.Println(*s)
-        re_crlf := regexp.MustCompile(`(\r\n\r\n)|(\n\n)|(\r\r)`)
-        *s = strings.TrimLeft(*s, `\r\n`)
-        crlf_pos := re_crlf.FindStringIndex(*s)
-        log.Println(crlf_pos[0])
+	log.Println(*s)
+	re_crlf := regexp.MustCompile(`(\r\n\r\n)|(\n\n)|(\r\r)`)
+	*s = strings.TrimLeft(*s, `\r\n`)
+	crlf_pos := re_crlf.FindStringIndex(*s)
+	log.Println(crlf_pos[0])
 
 }
 
 func main() {
-        fmt.Println("test")
-        addr := &net.UDPAddr{
-                IP:   net.ParseIP("127.0.0.1"),
-                Port: 5060,
-        }
-        sock, err := net.ListenUDP("udp", addr)
-        if err != nil {
-                log.Fatalln(err)
-                os.Exit(1)
-        }
-        buf := make([]byte, 0xffff)
-        for {
-                len, addr, err := sock.ReadFromUDP(buf)
-                if err == nil {
-                        s := string(buf[:len])
-                        log.Println(addr)
-                        msg := NewMessage()
-                        msg.Parse(&s, addr)
-                }
-        }
+	fmt.Println("test")
+	addr := &net.UDPAddr{
+		IP:   net.ParseIP("127.0.0.1"),
+		Port: 5060,
+	}
+	sock, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+	buf := make([]byte, 0xffff)
+	for {
+		len, addr, err := sock.ReadFromUDP(buf)
+		if err == nil {
+			s := string(buf[:len])
+			log.Println(addr)
+			msg := NewMessage()
+			msg.Parse(&s, addr)
+		}
+	}
 }
